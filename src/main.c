e83e16f0cdec71e2938425263e65f0b418ae2833
Rayaan <rayaanahmad316@gmail.com>
2023-05-24 16:51:56 -0500

    First commit

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include "raylib.h"
+
+int main() {
+
+
+    int screenWidth = 800;
+    int screenHeight = 800;
+
+    InitWindow(screenWidth, screenHeight, "Tic Tac Toe");
+    
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose()) {
+    
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+
+
+    }
+
+    
+       
+    CloseWindow();
+
+
+    return 0;
+}
